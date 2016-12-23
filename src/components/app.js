2b1d5be105210da1c48a07a9406289f182d2e4cd
MKS-Student <mks.student@hackreactor.com>
2016-12-23 15:57:59 -0800

    add Game & Choice components

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import UserStats from './user_stats.js';
 
+import Game from './game.js'
+
 export default class App extends Component {
   render() {
     // Render test for UserStats; it must be visited from a nav link later
     return (
-      <div>React simple started hey!
+      <div>
+        <Game />
         <UserStats />
       </div>
     );

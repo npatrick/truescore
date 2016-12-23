a08f4a9c1d62432d89073e04de8244ce7492136f
bartekringwelski <bartekr@gmail.com>
2016-12-23 11:19:50 -0800

    moved methods to handlers.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(morgan('dev'));
 
 // serve static files
 
-app.use('/', express.static(path.join(__dirname, '../client')));
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // parse requests
 app.use(bodyParser.json());
@@ -29,42 +29,17 @@ app.use(bodyParser.urlencoded({
 // ============TEMPORARY TO MOVE TO HANDLERS OR DB ====================//
   var arrayOfObjects = ["iceCream", "cheeseSteak", "fries", "Doner Kebab"];
 
-  var battlePairs = [];
-  var battlePairMaker = function() {
-    for (var i = 0; i<arrayOfObjects.length; i++){
-      for (var j = i+1; j<arrayOfObjects.length; j++){
-        battlePairs.push([arrayOfObjects[i], arrayOfObjects[j]]);
-      }
-    }
-    return battlePairs;
-  };
-  battlePairMaker(arrayOfObjects);
-  console.log(battlePairs);
-
-  var i = 0;
-
-
-
 // hardcoded storage items for now
   var storage = {iceCream:0, cheeseSteak:0, fries:0, "Doner Kebab":0 };
 
-// read storage object and return ranked list
-  var rankedArray = function(objectWithValues) {
-    var sortable = [];
-      for (var object in storage) {
-        sortable.push([object, storage[object]])
-        sortable.sort(function(a, b) {
-          return a[1] - b[1]
-        })
-      }
-  return sortable;
+  var battlePairs = handlers.battlePairMaker(arrayOfObjects);
 
-  }
+  var i = 0;
 
   // ============ END TEMPORARY TO MOVE TO HANDLERS OR DB ====================//
 
-  app.get('/nextBattlePairs', (req, res) => {
 
+  app.get('/nextBattlePairs', (req, res) => {
     res.send(battlePairs[i]);
     i++;
 
@@ -74,11 +49,11 @@ app.use(bodyParser.urlencoded({
 app.post('/resultOfBattle', (req, res) => {
 
   if(req.body.results[0].left == 1 && req.body.results[1].right == -1){
-    console.log("left wonnnnn!");
+    // console.log("left wonnnnn!");
     storage[ battlePairs[i-1][0] ]++
     storage[ battlePairs[i-1][1] ]--
   } else {
-    console.log("right wonnnn!");
+    // console.log("right wonnnn!");
     storage[ battlePairs[i-1][0] ]--
     storage[ battlePairs[i-1][1] ]++
   }
@@ -86,7 +61,9 @@ app.post('/resultOfBattle', (req, res) => {
 });
 
 app.get('/getRankList', (req, res) => {
-  res.send(rankedArray(storage));
+  //get storage from 
+
+  res.send(handlers.rankedArray(storage));
 });
 
 app.get('/showUserStats', (req, res) => {

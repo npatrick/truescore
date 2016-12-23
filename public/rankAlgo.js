a6b0246bb2df30f8fb2391cf8297dd1c93d502f2
bartekringwelski <bartekr@gmail.com>
2016-12-22 17:52:20 -0800

    additional test category added

--- public/rankAlgo.js
+++ public/rankAlgo.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 
 //setup and storage
-  var arrayOfObjects = ["iceCream", "cheeseSteak", "fries"];
+  var arrayOfObjects = ["iceCream", "cheeseSteak", "fries", "Doner Kebab"];
 
 //create array of battle pairs
   var battlePairs = [];
@@ -19,7 +19,7 @@ $( document ).ready(function() {
 
 
 // hardcoded storage items for now
-  var storage = {iceCream:0, cheeseSteak:0, fries:0};
+  var storage = {iceCream:0, cheeseSteak:0, fries:0, "Doner Kebab":0 };
 
 // read storage object and return ranked list
   var rankedArray = function(objectWithValues) {

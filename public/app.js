8abb4f5bff075886b36db80549a89b901da2f406
bartekringwelski <bartekr@gmail.com>
2016-12-23 17:53:09 -0800

    seeded db with cats and clean up

--- public/app.js
+++ public/app.js
@@ -2,14 +2,14 @@ $( document ).ready(function() {
 
 // get two comparisons at page load
  $.get('/nextBattlePairs', function(data){
-   $('#divA').append(data[0]);
-   $('#divB').append(data[1]);
+   $('#divA').append(data[0].name);
+   $('#divB').append(data[1].name);
  });
 
 
   $('#buttonA').on('click', function(){
     //post results to server
-    $.post('resultOfBattle', {results: [{left:1}, {right: -1}]} );
+    $.post('/updateDBwithResultOfBattle', {winner: "left"} );
 
     //update rank list
     $.get('/getRankList', function(data){
@@ -23,8 +23,8 @@ $( document ).ready(function() {
 
     // request new battle and populate
     $.get('/nextBattlePairs', function(data){
-      $('#divA').append(data[0]);
-      $('#divB').append(data[1]);
+      $('#divA').append(data[0].name);
+      $('#divB').append(data[1].name);
     });
   });
 
@@ -32,7 +32,7 @@ $( document ).ready(function() {
 
   $('#buttonB').on('click', function(){
     //post results to server
-    $.post('resultOfBattle', {results: [{left:-1}, {right: 1}]} );
+    $.post('/updateDBwithResultOfBattle', {winner: "right"});
 
     //update rank list
     $.get('/getRankList', function(data){
@@ -46,8 +46,8 @@ $( document ).ready(function() {
 
     // request new battle and populate
     $.get('/nextBattlePairs', function(data){
-      $('#divA').append(data[0]);
-      $('#divB').append(data[1]);
+      $('#divA').append(data[0].name);
+      $('#divB').append(data[1].name);
     });
 });
 

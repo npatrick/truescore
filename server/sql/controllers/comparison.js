1e1bc8b1e908bbc32cfc7bab9cbbbfb4673adbca
Michael Thelen <michael.thelen@gmail.com>
2017-01-05 18:25:59 -0800

    return next comparison with stats

--- server/sql/controllers/comparison.js
+++ server/sql/controllers/comparison.js
@@ -2,19 +2,68 @@ var db = require('../db/db_index');
 
 module.exports = {
   get: function(req, res){
+    const promptId = req.params.id || 1;
+    //how many choices in db?
     db.Comparison.count().
     then(count => {
-
-    const choice1Index = randomIdx(count);
-    let choice2Index = randomIdx(count);
-
-    while (choice1Index === choice2Index ){
-      choice2Index = randomIdx(count);
-    }
     
+    //get 2 random, but different ids
+    const choice1Id = randomId(count);
+    let choice2Id = randomId(count);
 
-    db.Choice.findAll( {where: {id: [choice1Index, choice2Index] } } )
-    .then(choices => res.send(choices));
+    while (choice1Id === choice2Id ){
+      choice2Id = randomId(count);
+    }
+
+    // get choices data from db
+    db.Choice.findAll( {where: {id: [choice1Id, choice2Id] } } )
+    .then(([choiceA, choiceB]) => {
+    
+      console.log("ChoiceA is ", choiceA.dataValues);
+
+      db.Comparison.count({where: {
+        winnerId: choiceA.id,
+        promptId
+      }})
+      .then((wins)=> {
+        choiceA.dataValues.wins = wins;
+      });
+
+      db.Comparison.count({where: {
+        loserId: choiceA.id,
+        promptId
+      }})
+      .then((losses)=> {
+        choiceA.dataValues.losses = losses;
+      });
+
+      db.Comparison.count({where: {
+        winnerId: choiceB.id,
+        promptId
+      }})
+      .then((wins)=> {
+        choiceB.dataValues.wins = wins;
+      });
+
+      db.Comparison.count({where: {
+        loserId: choiceB.id,
+        promptId
+      }})
+      .then((losses)=> {
+        choiceB.dataValues.losses = losses;
+        res.send([choiceA, choiceB]);
+      });
+
+
+
+      // choices[0].wins = cat1winner;
+      // choices[0].losser = cat1loses;
+      // choices[1].wins = cat2winner;
+      // choices[1].losses = cat
+      //res.send([choiceA, choiceB]);
+
+    
+    });
 
 
     });
@@ -36,6 +85,6 @@ module.exports = {
 }
 
 
-function randomIdx (itemsInSet){
-  return Math.floor( Math.random() * itemsInSet )
+function randomId (itemsInSet){
+  return Math.ceil( Math.random() * itemsInSet );
 }

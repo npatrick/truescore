246b01687e6ac834d8212f967decfebf6ea6cbed
Michael Thelen <michael.thelen@gmail.com>
2017-01-07 15:10:31 -0800

    shows results on card

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
-import Game from './components/game.js';
 import UserStats from './components/user_stats.js';
 import CompareChoices from './containers/compare_choices';
 import StatsListItem from './components/stats_list_item.js';

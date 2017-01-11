4bdf21d08d973e8da0b2983dfea5da6fe2d88e5a
nikshalavelayutham <niky.vel@gmail.com>
2017-01-11 11:53:00 -0800

    Changed prompt icons

--- docs/db_stubs/prompts.js
+++ docs/db_stubs/prompts.js
@@ -1,17 +1,17 @@
 module.exports = [
   {
-    "tileImage":"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQnEqDHtCpNwGBp3wDD9tkNdW3b8LjFFTDAQPpNWL2kWwxzlZLV",
+    "tileImage":"https://static1.squarespace.com/static/5233f116e4b0b842898f25d7/t/56d37da3c2ea51b152124420/1456700853032/",
     "text": "Invent the next snapchat",
     "sensitive": false,
 
   },
   {
-    "tileImage":"http://simpleicon.com/wp-content/uploads/smile.png",
+    "tileImage":"http://davidpapp.com/wp-content/uploads/2014/07/spy.png",
     "text": "Get arrested by the NSA",
     "sensitive": false
   },
   {
-    "tileImage":"http://www.thebenjaminswedding.com/img/drinks.png",
+    "tileImage":"https://s-media-cache-ak0.pinimg.com/originals/e2/04/23/e2042311f00b753704995768c863efc4.jpg",
     "text": "Code remotely from the beach",
     "sensitive": false
   }

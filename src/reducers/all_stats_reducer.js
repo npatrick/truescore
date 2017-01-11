96b15a867222b63b63b14896bdc58aeb31a6a1a1
Michael Thelen <michael.thelen@gmail.com>
2017-01-11 09:51:05 -0800

    add allSTATS to state, collects stats per prompt

---
+++ src/reducers/all_stats_reducer.js
@@ -0,0 +1,19 @@
+import { FETCH_STATS_BY_PROMPT } from '../actions/actions';
+
+const INITIAL_STATE = {};
+
+export default function(state = INITIAL_STATE, action) {
+  switch( action.type ) {
+
+    case FETCH_STATS_BY_PROMPT:
+      const newState = {...state};
+      const {promptId, stats} = action.payload.data;
+
+      newState[promptId] = stats;
+      console.log("NEW allStats STATE: ", newState);
+      return newState;
+
+    default:
+      return state;
+  }
+}

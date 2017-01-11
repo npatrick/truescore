bf9b9d14fd5e6b81682e63aaa70e9353b3e3c98e
Michael Thelen <michael.thelen@gmail.com>
2017-01-10 16:29:16 -0800

    test route with raw query

--- server/sql/controllers/setup.js
+++ server/sql/controllers/setup.js
@@ -41,7 +41,21 @@ module.exports = {
   testroute: {
     get: function(req, res){
       
-      res.send('testing route!');
+      // db.database.query(`select winnerId, count(*) from comparisons 
+      // where promptId = 1 group by winnerId`)
+      // .spread((results, metadata) => res.send(results))
+
+
+      db.database.query(`select c.id, c.name, w.wins, l.losses from 
+      choices as c left join 
+      (select winnerId, count(*) as wins from comparisons where promptId = 1 group by winnerId)
+      as w on c.id = w.winnerId
+      left join 
+      (select loserId, count(*) as losses from comparisons where promptId = 1 group by loserId)
+      as l on c.id = l.loserId`)
+      .spread((results, metadata) => res.send(results))
+
+  
 
     }
   
